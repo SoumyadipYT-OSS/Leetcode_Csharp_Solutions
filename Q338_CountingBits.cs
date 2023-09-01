bb89c3818895c30c2e676417287d9873ebb577c5
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2023-09-01 10:54:16 +0530

    Add files via upload

---
+++ Q338_CountingBits.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] CountBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            result[i] = result[(i & (i - 1))] + 1;
+        }
+        return result;
+    }
+}
