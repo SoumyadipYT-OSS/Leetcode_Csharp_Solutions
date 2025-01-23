10d2db4ef6911d85f8c5626e2e7172cf5516df0b
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2025-01-23 12:49:57 +0530

    Create Q1267_CountServersCommunicate.cs

---
+++ Q1267_CountServersCommunicate.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int CountServers(int[][] grid) {
+        int m = grid.Length; 
+        int n = grid[0].Length; 
+
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
