066a696ef6324044c3bc76cb710cd92e4d794726
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2025-02-02 19:32:50 +0530

    Create Q1752_Check_array_sorted_and_rotated.cs

---
+++ Q1752_Check_array_sorted_and_rotated.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool Check(int[] nums) {
+        int n = nums.Length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {   
+        if (nums[i] > nums[(i + 1) % n])   
+            count++; 
+    }
+    return count <= 1;
+    }
+}
